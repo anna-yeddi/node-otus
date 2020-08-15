679105ca140944deb2912e5ea7c97f1793cc1839
Anna Kova <m3ta007@gmail.com>
2020-08-14 20:20:58 -0400

    Tree function is created

---
+++ javascript/tree-js/index.js
@@ -0,0 +1,35 @@
+function tree(nameItemsObj, indent) {
+  for (const [key, value] of Object.entries(nameItemsObj)) {
+    // Print out name
+    if (key === 'name') {
+      console.log(indent, value)
+    }
+
+    // Indent items
+    if ((key === 'items') & Array.isArray(value)) {
+      let indentItem = ' ├——',
+        indentChild = ' │   └——'
+      indent = indent === indentItem ? indentChild : indentItem
+
+      value.map((item) => {
+        tree(item, indent)
+      })
+    }
+  }
+}
+
+const makeTreeObj = {
+  name: 1,
+  items: [
+    {
+      name: 2,
+      items: [{ name: 3 }, { name: 4 }],
+    },
+    {
+      name: 5,
+      items: [{ name: 6 }],
+    },
+  ],
+}
+
+tree(makeTreeObj, '')
