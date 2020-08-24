b7afab56b316e0cf878271f3cf53a733708ead40
Anna Kova <m3ta007@gmail.com>
2020-08-23 20:40:40 -0400

    Tree function for JS is updated with better indentation

--- node/tree/tree.js
+++ node/tree/tree.js
@@ -1,11 +1,54 @@
 const fs = require('fs')
 
+// Check user input
+if (process.argv.length <= 2) {
+  console.log(
+    'Error in executing: ' +
+      __filename +
+      '\n Include path/to/directory and optional flag (-d, --depth) with tree depth in number'
+  )
+  process.exit(-1)
+}
+
+const path = process.argv[2]
+const depth =
+  (process.argv[3] === '-d' || process.argv[3] === '--depth') &&
+  Number(process.argv[4])
+
 function tree(path, depth = 0) {
+  async function print(path, indent) {
+    // const dir = await fs.promises.opendir(path).catch(console.error)
+    const dir = await fs.promises
+      .readdir(path, { withFileTypes: true })
+      .catch(console.error)
+
+    console.log(dir, 'for path and indent: ', path, indent)
+    // Print out name
+    console.log(indent, dir.name)
+
+    for await (const dirent of dir) {
+      // Check and structure indentation
+      const INDENT_CORNER = ' \u251c\u2014\u2014'
+      const INDENT_VERTICAL = ' \u2502  '
+      const nextIndent =
+        indent === ''
+          ? (indent += INDENT_CORNER)
+          : (indent = INDENT_VERTICAL + indent)
+
+      let nextPath = path + '/' + dirent.name
+      // dirent.isFile() && print(nextPath, indent)
+      dirent.isDirectory() && print(nextPath, nextIndent)
+    }
+  }
+
+  print(path, (indent = '')).catch(console.error)
+  // }
   // async function print(path, indent = '') {
-  //   const dir = await fs.promises.opendir(path)
-  //   for await (const dirent of dir) {
+  //   const fileEntries = await fs.promises.readdir(path, { withFileTypes: true })
+
+  //   for await (const dirent of fileEntries) {
   //     // Print out name
-  //     console.log(indent, dirent.name)
+  //     console.log(indent, dirent.name, dirent, path)
 
   //     // Check and structure indentation
   //     let indentItem = ' \u251c\u2014\u2014',
@@ -19,25 +62,32 @@ function tree(path, depth = 0) {
   //     dirent.isDirectory() && print(nextPath, nextIndent)
   //   }
   // }
-  async function print(path, indent = '') {
-    const fileEntries = await fs.promises.readdir(path, { withFileTypes: true })
+  // async function print(path, indent = '') {
+  //   const fileEntries = await fs.promises.readdir(path, { withFileTypes: true })
 
-    for await (const dirent of fileEntries) {
-      // Print out name
-      console.log(indent, dirent.name, dirent, path)
+  //   console.log(indent, fileEntries.name)
+  //   let dirNames = fileEntries
+  //     .filter((dir) => dir.isDirectory())
+  //     .map((dir) => console.log(indent, dir.name))
+  //   let fileNames = fileEntries
+  //     .filter((file) => file.isFile())
+  //     .map((file) => console.log(indent, file.name))
 
-      // Check and structure indentation
-      let indentItem = ' \u251c\u2014\u2014',
-        indentChild = ' \u2502  ',
-        nextIndent =
-          indent === ''
-            ? (indent += indentItem)
-            : (indent = indentChild + indent)
+  //   console.log(dirNames, fileNames)
 
-      let nextPath = path + '/' + dirent.name
-      dirent.isDirectory() && print(nextPath, nextIndent)
-    }
-  }
+  //   // for await (const dirent of fileEntries) {
+  //   //   // Print out name
+  //   //   console.log(indent, dirent.name, dirent, path)
+
+  //   //   // Check and structure indentation
+  //   let indentItem = ' \u251c\u2014\u2014',
+  //     indentChild = ' \u2502  ',
+  //     nextIndent =
+  //       indent === '' ? (indent += indentItem) : (indent = indentChild + indent)
+
+  //   let nextPath = path + '/' + dirent.name
+  //   dirent.isDirectory() && print(nextPath, nextIndent)
+  // }
 
   // let fileEnts = await fs.promises.readdir(path, { withFileTypes: true })
 
@@ -47,8 +97,6 @@ function tree(path, depth = 0) {
 
   // console.log(fileNames)
   // }
-
-  print(path).catch(console.error)
 
   // let level = 0
   // while (level < depth) {
@@ -87,5 +135,6 @@ function tree(path, depth = 0) {
 }
 
 // tree('../../', 2) // repo root
-tree('..', 2) // node dir
-// tree('.', 2) // tree dir
+// tree('../../node', 2) // node dir
+// tree('../tree', 2) // tree dir
+tree(path, depth)
